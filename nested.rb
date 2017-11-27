42f378f4a13a0baa9266678262f85c5a1b9ebc09
Chris Gullickson <cgullickson5@gmail.com>
2017-11-27 17:28:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,24 +79,6 @@ end
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-
-end
 
 def changing_alan
 	programmer_hash =
