db089aa3d89a2c22aa77ea11a16922cf2be67e12
Chris Gullickson <cgullickson5@gmail.com>
2017-11-27 17:27:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,8 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
 
+return programmer_hash[:yukihiro_matsumoto]
+
 
 end
 
